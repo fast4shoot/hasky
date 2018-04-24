6e8d0dc01cd201668fcfa6de368e4704cda594be
Zdeněk Biberle <z.biberle@gmail.com>
2018-04-24 12:16:58 +0200

    Improved builtins

--- source/Builtins.hs
+++ source/Builtins.hs
@@ -1,49 +1,49 @@
-{-# LANGUAGE OverloadedStrings, LambdaCase #-}
+{-# LANGUAGE OverloadedStrings, LambdaCase, TypeApplications #-}
 module Builtins(builtins) where
 
 import Control.Arrow ((&&&))
 import qualified Data.Map.Strict as Map
+import Data.Semigroup ((<>))
 import qualified Data.Text as T
 
 import AST
 
+class FromVal a where fromVal :: Val b -> Either T.Text a
+class ToVal a where toVal :: T.Text -> a -> Val b
+
+instance FromVal Integer where
+    fromVal (VInt x) = Right x
+    fromVal x = Left "can't convert Val to Integer"
+
+instance ToVal Integer where toVal _ x = VInt x
+instance ToVal Bool where toVal _ x = builtinBool x
+
+instance (FromVal a, ToVal b) => ToVal (a -> b) where
+    toVal name f = VBuiltin name $ \x -> do
+        x' <- x
+        val <- fromVal x'
+        pure $ toVal (name <> ".fn") $ f val
+
+fn2 name f = VBuiltin name $ \x -> pure $ VBuiltin (name <> ".fn") $ \y -> f x y
 builtinTrue = fn2 "true" $ \t _ -> t
 builtinFalse = fn2 "false" $ \_ e -> e
 builtinBool c = if c then builtinTrue else builtinFalse
-
-intErrMsg name = Left $ T.concat ["function ", name, " is only callable on numbers"]
-
-fn1ii name f = VBuiltin name $ \case
-    Right (VInt a) -> pure $ VInt $ f a
-    Right _ -> intErrMsg name
-    x -> x
-
-fn2 name f = VBuiltin name $ \a -> pure $ VBuiltin (T.append name ".fn") $ \b -> f a b
-fn2ii name f = VBuiltin name $ \case
-    Right (VInt a) -> pure $ VBuiltin (T.append name ".fn") $ \case
-        Right (VInt b) -> pure $ f a b
-        Right _ -> intErrMsg name
-        x -> x
-    Right _ -> intErrMsg name 
-    x -> x
-fn2iii name f = fn2ii name $ \a b -> VInt $ f a b
-fn2iib name f = fn2ii name $ \a b -> builtinBool $ f a b
 
 builtins :: Map.Map [T.Text] (ParsedModule a)
 builtins = Map.singleton ["Builtin"] (ParsedModule [] decls)
     where
         decls = fmap (vGetBuiltinName &&& PEVal) [
-            fn2iii "+" (+),
-            fn2iii "-" (-),
-            fn2iii "*" (*),
-            fn2iii "/" div,
-            fn2iii "%" mod,
-            fn2iib "==" (==),
-            fn2iib "!=" (/=),
-            fn2iib "<" (<),
-            fn2iib ">" (>),
-            fn2iib "<=" (<=),
-            fn2iib ">=" (>=),
-            fn1ii "succ" succ,
-            fn1ii "pred" pred
+            toVal "+" $ (+) @Integer,
+            toVal "-" $ (-) @Integer,
+            toVal "*" $ (*) @Integer,
+            toVal "/" $ div @Integer,
+            toVal "%" $ mod @Integer,
+            toVal "==" $ (==) @Integer,
+            toVal "!=" $ (/=) @Integer,
+            toVal "<" $ (<) @Integer,
+            toVal ">" $ (>) @Integer,
+            toVal "<=" $ (<=) @Integer,
+            toVal ">=" $ (>=) @Integer,
+            toVal "succ" $ succ @Integer,
+            toVal "pred" $ pred @Integer
             ]
